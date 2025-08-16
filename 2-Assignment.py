44d00400e5b88d9a38551be324e5e004fff4f607
asthakaradwar2005-ai <asthakaradwar2005@gmail.com>
2025-08-16 17:44:16 +0530

    Add files via upload

---
+++ 2-Assignment.py
@@ -0,0 +1,7 @@
+a=("Enter your first name=Anurag")
+b=("Enter your last name=Singh")
+print('Hello,Anurag Singh! Welcome to the Python Program')
+
+
+
+
---
+++ Assignment.py
@@ -0,0 +1,6 @@
+a=8
+b=6 
+print(8+6)
+print(8-6)
+print(8*6)
+print(8/6)
