a88e0053e39ce2cb694300d08d2e702c7524505c
asthakaradwar2005-ai <asthakaradwar2005@gmail.com>
2025-08-17 22:42:37 +0530

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,6 +1,12 @@
-a=8
-b=6 
-print(8+6)
-print(8-6)
-print(8*6)
-print(8/6)
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+print("Addition :",  num1 + num2)
+print("Subtraction :", num1 - num2)
+print("Multiplication :", num1 * num2)
+
+if num2 != 0 :
+    print("Division:", num1 // num2)
+
+else :
+    print("Error ! Division by zero.")
